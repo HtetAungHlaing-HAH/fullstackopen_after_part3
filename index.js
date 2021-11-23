b16501365317cb75e1ae5363b9b9e89df4639162
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-11-24 00:53:48 +0630

    Ex 3.15

--- index.js
+++ index.js
@@ -54,10 +54,14 @@ app.get('/api/persons/:id', (request, response) => {
 
 //Delete resources
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).end()
+    })
 })
 
 //to generate randome id
