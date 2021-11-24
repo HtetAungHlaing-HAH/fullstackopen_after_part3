489661b9df8dd696b097e9bb4bba61c9f5e38de6
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-11-24 23:38:12 +0630

    Ex 3.17

--- index.js
+++ index.js
@@ -49,7 +49,14 @@ app.get('/info', (request, response) => {
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
   .then(person => {
-    response.json(person)
+    if(person)
+    {
+      response.json(person)
+    }
+    else
+    {
+      response.status(404).end()
+    }
   })
   .catch(error => next(error))
 })
@@ -90,6 +97,21 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 //unknown endpoint middleware
