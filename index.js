22f6c164ee13c179eb90361810e6258220118c12
Htet Aung Hlaing <htetaunghlaing248@gmail.com>
2021-11-28 21:54:49 +0630

    Ex 3.19

--- index.js
+++ index.js
@@ -77,26 +77,19 @@ const randomId =(max) => {
 }
 
 //Create new resource
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  if(body.name === undefined)
-  {
-    return response.status(400).json({error: 'name missing'})
-  }
-  else if(body.number === undefined)
-  {
-    return response.status(400).json({error: 'number missing'})
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 //update resources
@@ -137,7 +130,12 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError')
   {
-    response.status(400).send({ error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id'})
+  }
+  else if(error.name === 'ValidationError')
+  {
+    console.log(error.name)
+    return response.status(400).send({error: error.message})
   }
 
   next(error)
